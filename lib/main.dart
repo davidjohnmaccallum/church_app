2d444d40175431e5f5981c6db4cf7e626ff7ff59
David MacCallum <david.maccallum@takealot.com>
2021-02-13 21:00:56 +0200

    Media list

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:church_app/media_list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MediaList(),
+    );
+  }
+}
---
+++ lib/media_list.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class MediaList extends StatelessWidget {
+  const MediaList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Media List"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+          ListTile(
+            leading: Image(image: NetworkImage("https://picsum.photos/200")),
+            title: Text("Et voluptate"),
+            subtitle: Text("Minim nulla Lorem laborum qui amet."),
+            trailing: Icon(Icons.play_arrow),
+          ),
+        ],
+      ),
+    );
+  }
+}
