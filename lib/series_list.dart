82309add09ecb51e96b96c4fe31963def9fe5dd5
David MacCallum <davidjohnmac@gmail.com>
2021-05-08 22:38:16 +0200

    Tweaking fade in images

--- lib/series_list.dart
+++ lib/series_list.dart
@@ -45,9 +45,13 @@ class SeriesList extends StatelessWidget {
 
   ListTile buildListTile(String title, String subtitle, String imageUrl, Function() onTap) {
     return ListTile(
-      leading: FadeInImage.memoryNetwork(
-        placeholder: kTransparentImage,
-        image: imageUrl,
+      leading: SizedBox(
+        height: 56.0,
+        width: 56.0,
+        child: FadeInImage.memoryNetwork(
+          placeholder: kTransparentImage,
+          image: imageUrl,
+        ),
       ),
       title: Text(title),
       subtitle: Text(subtitle),
@@ -68,29 +72,32 @@ class ListHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomLeft,
-      children: [
-        SizedBox(
-          height: 200,
-          width: double.infinity,
-          child: FittedBox(
-            fit: BoxFit.cover,
-            clipBehavior: Clip.hardEdge,
-            child: FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: imageUrl,
+    return Container(
+      color: Colors.grey,
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: FittedBox(
+              fit: BoxFit.cover,
+              clipBehavior: Clip.hardEdge,
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: imageUrl,
+              ),
             ),
           ),
-        ),
-        Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 60,
-          ),
-        )
-      ],
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 60,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
